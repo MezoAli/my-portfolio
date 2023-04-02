9655ef3984f7668d1e8a4e6f1d5f3555af6a9b72
Moutaz Ali <moutaz.ali.dev@gmail.com>
2023-04-03 01:46:46 +0200

    adding navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,31 @@
+import Link from "next/link";
+
 function Navbar() {
-	return <header>Navbar</header>;
+	return (
+		<header className="w-full px-32 py-8 font-medium flex justify-between items-center">
+			<nav>
+				<Link href="/">Home</Link>
+				<Link href="/about">About</Link>
+				<Link href="/projects">Projects</Link>
+				<Link href="/contact">Contact</Link>
+			</nav>
+			<h2>Logo</h2>
+			<nav>
+				<Link href="" target="_blank">
+					F
+				</Link>
+				<Link href="" target="_blank">
+					L
+				</Link>
+				<Link href="" target="_blank">
+					G
+				</Link>
+				<Link href="" target="_blank">
+					T
+				</Link>
+			</nav>
+		</header>
+	);
 }
 
 export default Navbar;
